a4296a3d5552f0b3caa2af709640b0203025c5d3
Agustin Fernandez <afernandezmariscal233@alumno.unlam.edu.ar>
2025-10-28 22:53:42 -0300

    Otro SP de importacion creado. Leer comentarios

--- CreacionTablas.sql
+++ CreacionTablas.sql
@@ -1,7 +1,14 @@
+-- NOTAS !!!!!!
+-- LEER LOS COMENTARIOS
+-- EL ULTIMO SP (sp_importar_uf) no importa bien el "coeficiente", no sé como arreglarlo
+-- ALGUNAS VECES CUANDO SE RESETEA EL IDENTITY DSP EMPIEZA EN 0 (Y NO EN 1, COMO DEBERIA) NO SE PQ, SI ALGUNO SABE BIEN SINO LE PREGUNTAMOS AL PROFE
+-- TODO LO QUE ESTA ACA ANDA (POR AHI HAYA QUE MODIFICAR COSAS) PERO SI NO LES "COMPILA" O TIENEN ERRORES AVISEN Y LO VEMOS, PQ LO PROBE Y ANDA TODO
+
 create database Com5600G02
 go 
 
 use Com5600G02
+go
 
 --Hay que crear schemas??
 
@@ -41,10 +48,10 @@ create table pago (
 go
 
 create table consorcio (
-	id_consorcio int primary key,
+	id_consorcio int identity(1,1) primary key,
 	nombre varchar(35),
 	direccion varchar(35),
-	cant_UF int,
+	cant_uf int,
 	cant_m2 int,);
 go
 
@@ -62,28 +69,30 @@ go
 create table estadoFinanciero (
 	id int,
 	id_consorcio int,
-	saldo_Anterior decimal(10,2),
+	saldo_anterior decimal(10,2),
 	ingreso_expensas_termino decimal(10,2),
 	ingreso_expensas_adeudadas decimal(10,2),
 	ingreso_expensas_adelantadas decimal(10,2),
 	egreso decimal(10,2),
-	saldo_Cierre decimal(10,2),
+	saldo_cierre decimal(10,2),
 	periodo date,
 	primary key (id, id_consorcio),
 	constraint fk_estadoFinanciero_id_consorcio foreign key (id_consorcio) references consorcio (id_consorcio));
 go
 
 create table unidadFuncional (
-	id_uf int primary key,
-	id_consorcio int,
+	id_uf int identity(1,1) primary key, --LE AGREGE EL IDENTITY(1,1) POR QUE SINO, NO PODIA IMPORTAR Y HACER CC ERA MUY COMPLEJO PARA TESTAR
+	id_consorcio int, -- fk
 	cuenta_origen varchar(50),
 	numero_uf int,
 	piso varchar(3),
 	depto varchar(5),
 	cochera bit,
+	cochera_m2 int,
 	baulera bit,
+	baulera_m2 int,
 	cant_m2 int,
-	--Coeficiente ????
+	coeficiente decimal (5,2),
 	constraint fk_uf_id_consorcio foreign key (id_consorcio) references consorcio (id_consorcio)
 	);
 go
@@ -125,16 +134,16 @@ create table estadoCuentaProrrateo (
 	id_expensa int,
 	id_uf int,
 	id_pago int,
-	fechaEmision date,
-	fecha1erVenc date,
-	fecha2doVenc date,
-	saldoAnterior decimal(10,2),
-	pagosRecibidos decimal(10,2),
+	fecha_emision date,
+	fecha_1er_venc date,
+	fecha_2do_venc date,
+	saldo_anterior decimal(10,2),
+	pagos_recibidos decimal(10,2),
 	deuda decimal(10,2),
-	interesPorMora decimal(10,2),
-	expensasOrdinarias decimal(10,2),
-	expensasExtraordinarias decimal(10,2),
-	totalPagar decimal(10,2),
+	interes_por_mora decimal(10,2),
+	expensas_ordinarias decimal(10,2),
+	expensas_extraordinarias decimal(10,2),
+	total_pagar decimal(10,2),
 	constraint fk_estadoCuentaProrrateo_id_expensa
 	foreign key (id_expensa) references expensa (id_expensa),
 	constraint fk_estadoCuentaProrrateo_id_uf
@@ -146,10 +155,10 @@ go
 create table gastoOrdinario (
 	id_gastoOrdinario int primary key,
 	id_gasto int,
-	--tipo_gasto ???
+	tipo_gasto varchar(50),
 	--subtipoGasto ???
-	nombreEmpresa varchar(50),
-	nroFactura varchar(50),
+	nombre_empresa varchar(50),
+	nro_factura varchar(50),
 	importe decimal(10,2)
 	constraint fk_gastoOrdinario_id_gasto 
 	foreign key (id_gasto) references gasto (id_gasto),
@@ -159,13 +168,13 @@ go
 create table gastoExtraordinario (
 	id_gastoExtraordinario int primary key,
 	id_gasto int,
-	--tipo_gasto ???
+	tipo_gasto varchar(50),
 	--subtipoGasto ???
-	nombreEmpresa varchar(50),
-	nroFactura varchar(50),
+	nombre_empresa varchar(50),
+	nro_factura varchar(50),
 	descripcion varchar(50),
-	nroCuota int,
-	totalCuotas int,
+	nro_cuota int,
+	total_cuotas int,
 	importe decimal(10,2)
 	constraint fk_gastoExtraordinario_id_gasto 
 	foreign key (id_gasto) references gasto (id_gasto),
@@ -174,6 +183,8 @@ go
 
 
 --STORED PROCEDURES
+
+--CARGAR PERSONAS
 
 IF OBJECT_ID('dbo.sp_importar_personas', 'P') IS NOT NULL
     DROP PROCEDURE dbo.sp_importar_personas;
@@ -199,7 +210,7 @@ BEGIN
     );
 
 	INSERT INTO persona (nombre, apellido, dni, email_personal, telefono_contacto, cuenta, inquilino)
-	SELECT UPPER(LTRIM(RTRIM(nombre))), UPPER(LTRIM(RTRIM(apellido))), dni, LOWER(REPLACE(LTRIM(RTRIM(email_personal)), ' ', '')), telefono_contacto, cuenta, inquilino
+	SELECT UPPER(LTRIM(RTRIM(nombre))), UPPER(LTRIM(RTRIM(apellido))), LTRIM(RTRIM(dni)), LOWER(REPLACE(LTRIM(RTRIM(email_personal)), ' ', '')), LTRIM(RTRIM(telefono_contacto)), REPLACE(LTRIM(RTRIM(cuenta)), ' ', ''), inquilino
 	FROM #tempPersona
 END;
 go
@@ -217,3 +228,96 @@ select * from persona --test para ver personas
 
 --TUVE QUE MODIFICAR EL ARCHIVO .CSV POR QUE HABIA UN DNI DUPLICADO Y ES UNIQUE ESE CAMPO (Y DEBE SERLO)
 --HABRIA QUE AGREGAR ALGO PARA QUE SI HAY UN DNI DUPLICADO LO IGNORE PARA QUE NO SALTE ERROR
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+--CARGAR CONSORCIOS
+IF OBJECT_ID('dbo.sp_importar_consorcios', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.sp_importar_consorcios;
+GO
+CREATE PROCEDURE sp_importar_consorcios
+AS
+BEGIN
+	CREATE TABLE #tempConsorcio (
+		num_consorcio varchar(12),
+		nombre varchar(35),
+		direccion varchar(35),
+		cant_uf int,
+		cant_m2 int,);
+
+    BULK INSERT #tempConsorcio
+    FROM '' -- <-- PONER LA RUTA DE ACCESO AL ARCHIVO (datos varios 1(Consorcios).csv) QUE TENGAN USTEDES (ES EL ARCHIVO EXCEL, TIENE DOS VENTANAS DENTRO, EXPORTAR COMO CSV)
+    WITH (
+        FIELDTERMINATOR = ';',
+        ROWTERMINATOR = '\n',
+        FIRSTROW = 2
+    );
+
+	INSERT INTO consorcio (nombre, direccion, cant_uf, cant_m2)
+	SELECT UPPER(LTRIM(RTRIM(nombre))), UPPER(LTRIM(RTRIM(direccion))), cant_uf, cant_m2
+	FROM #tempConsorcio
+END;
+go
+
+--EJECUTAR TODO JUNTO ESTO
+------------------- TESTING ---------------------
+delete from consorcio --test
+go
+DBCC CHECKIDENT ('consorcio', RESEED, 0); --Reincia el IDENTITY(1,1)
+go
+exec sp_importar_consorcios; --test
+go
+select * from consorcio --test para ver consorcios
+-------------------------------------------------
+
+
+--CARGAR UNIDADES FUNCIONALES
+IF OBJECT_ID('dbo.sp_importar_uf', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.sp_importar_uf;
+GO
+CREATE PROCEDURE sp_importar_uf
+AS
+BEGIN
+	-- MISMO ORDEN QUE EN EL ARCHIVO "UF por consorcio.txt"
+	CREATE TABLE #tempUf ( 
+		nombre_consorcio varchar(35),
+		numero_uf varchar(10),
+		piso varchar(10),
+		depto varchar(10),
+		coeficiente varchar(10),
+		uf_m2 varchar(10),
+		baulera varchar(10),
+		cochera varchar(10),
+		baulera_m2 varchar(10),
+		cochera_m2 varchar(10));
+
+    BULK INSERT #tempUf
+    FROM '' -- <-- PONER LA RUTA DE ACCESO AL ARCHIVO (UF por consorcio.txt) QUE TENGAN USTEDES
+    WITH (
+        FIELDTERMINATOR = '\t',
+        ROWTERMINATOR = '\n',
+        FIRSTROW = 2,
+		CODEPAGE = '65001' 
+    );
+
+	INSERT INTO unidadFuncional (id_consorcio, numero_uf, piso, depto, cochera, cochera_m2, baulera, baulera_m2, cant_m2, coeficiente)
+	SELECT	UPPER(c.id_consorcio), CAST(u.numero_uf AS INT), CAST(u.piso AS varchar(3)), CAST(u.depto as varchar(5)), 
+			CASE WHEN u.cochera = 'SI' THEN 1 ELSE 0 END, CAST(u.cochera_m2 AS INT), CASE WHEN u.baulera = 'SI' THEN 1 ELSE 0 END, 
+			CAST(u.baulera_m2 as INT), CAST(u.uf_m2 AS INT), CAST(REPLACE(u.coeficiente, ',', '.') AS decimal)
+	FROM #tempUf u
+	JOIN consorcio c ON c.nombre = u.nombre_consorcio;
+END;
+go
+
+--EJECUTAR TODO JUNTO ESTO
+------------------- TESTING ---------------------
+delete from unidadFuncional --test
+go
+DBCC CHECKIDENT ('unidadFuncional', RESEED, 0); --Reincia el IDENTITY(1,1)
+go
+exec sp_importar_uf; --test
+go
+select * from unidadFuncional --test para ver consorcios
+-------------------------------------------------
+-- TUVE QUE BORRAR LOS ULTIMOS RENGLONES VACIOS DEL ARCHIVO DE TXT PQ SINO NO FUNCIONABA (seguramente tengan que hacer lo mismo)
+-- OTRA COSA ES QUE LE PUSE IDENTITY(1,1) A unidadFuncional LO CUAL POR AHI ES MEDIO INCOMODO, PERO SINO ESTO NO FUNCIONA
